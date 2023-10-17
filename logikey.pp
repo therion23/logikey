6ca4c6ea2b69baa4263b0ec32249618b4432ed83
Bo Krogsgaard <therion23@users.noreply.github.com>
2023-10-18 00:34:47 +0200

    Add files via upload

---
+++ logikey.pp
@@ -0,0 +1,37 @@
+Program logikey;
+
+// Uses CRT;
+
+Var I: File;
+    B: Byte;
+    NR: Word;
+    Buf: Array[0..63] Of Byte;
+    LastVal: Byte;
+    Found: Boolean;
+
+Begin
+{$I-}
+  Assign(I, '/dev/logilink');
+  FileMode := 0;
+  Reset(I, 1);
+  FileMode := 2;
+{$I+}
+  If IOResult <> 0 Then WriteLn('Could not open device');
+  LastVal := 0;
+  While True Do Begin
+{$I-}
+    BlockRead(I, Buf[0], 64, NR);
+{$I+}
+    If IOResult <> 0 Then WriteLn('Error reading');
+//    If NR > 0 Then For B := 0 To NR Do If Buf[B] > 0 Then Write(Buf[B]);
+//    If NR > 0 Then WriteLn;
+    Found := False;
+    If NR > 0 Then For B := 0 To NR Do If Buf[B] > 0 Then Begin
+      LastVal := (Buf[B]);
+      Found := True;
+    End;
+    If Not Found Then Break;
+  End;
+  WriteLn(LastVal);
+End.
+
